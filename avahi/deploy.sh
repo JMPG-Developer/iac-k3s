967387b20ca5d0ac12362d9dfcd9cf6c79524212
Jose Miguel Perez <jmpg.dev@gmail.com>
2024-12-26 13:24:08 +0100

    Remove Avahi Configuration

--- avahi/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp ./kafka.service /etc/avahi/services/kafka.service
-cp ./schema-registry.service /etc/avahi/services/schema-registry.service
-cp ./control-center.service /etc/avahi/services/control-center.service
-systemctl restart avahi-daemon
